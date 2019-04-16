816113d60f8d4d1b4d160f9abca6a5a075b15109
Kirill Smirnov <smirnowku@gmail.com>
2019-04-17 01:57:15 +0300

    Make JPA tests transactional to keep context clean

--- src/test/java/tests/JpaSpringTest.java
+++ src/test/java/tests/JpaSpringTest.java
@@ -2,6 +2,9 @@ package tests;
 
 import org.springframework.test.context.ContextConfiguration;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @ContextConfiguration(classes = JpaTestConfig.class)
 public abstract class JpaSpringTest extends SpringTest {
 }
